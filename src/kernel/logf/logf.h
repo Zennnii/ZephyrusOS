b54ac85a51acb1dc3b9eadf88ae9211d35c748db
VukProgramming <vukmitic81@gmail.com>
2025-08-29 17:03:33 +0200

    Added a load log macro

--- src/kernel/logf/logf.h
+++ src/kernel/logf/logf.h
@@ -7,8 +7,9 @@
 
 // Colors
 #define LOG_COLOR_INFO  COLOR8_LIGHT_GREEN
-#define LOG_COLOR_WARN  COLOR8_YELLOW
-#define LOG_COLOR_ERR   COLOR8_LIGHT_RED
+#define LOG_COLOR_WARN  COLOR8_LIGHT_RED
+#define LOG_COLOR_ERR   COLOR8_RED
+#define LOG_COLOR_LOAD COLOR8_LIGHT_BROWN
 
 // Logger
 void klog(const char* level, char* msg, uint16_t color, uint8_t nlflag);
@@ -16,9 +17,11 @@ void klog(const char* level, char* msg, uint16_t color, uint8_t nlflag);
 #define LOG_INFO(msg)  klog("INFO", msg, LOG_COLOR_INFO, 1)
 #define LOG_WARN(msg)  klog("WARN", msg, LOG_COLOR_WARN, 1)
 #define LOG_ERR(msg)   klog("ERR", msg, LOG_COLOR_ERR, 1)
+#define LOG_LOAD(msg) klog("LOAD", msg, LOG_COLOR_LOAD, 1)
 
 #define LOG_INFO_NONL(msg)  klog("INFO", msg, LOG_COLOR_INFO, 0)
 #define LOG_WARN_NONL(msg)  klog("WARN", msg, LOG_COLOR_WARN, 0)
 #define LOG_ERR_NONL(msg)   klog("ERR", msg, LOG_COLOR_ERR, 0)
+#define LOG_LOAD_NONL(msg) klog("LOAD", msg, LOG_COLOR_LOAD, 0)
 
 #endif
